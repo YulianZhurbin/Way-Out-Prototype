b0380d3c67710096d9f1a5a2ca4ff247c24486c6
Julian <julianzhurbin2015@gmail.com>
2022-08-25 20:27:09 +0300

    Models set

---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Threading;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject prisonCamera;
+    private bool isPrisonCameraUsed;
+    private bool isFirstFrame = true;
+    Mouse mouse;
+    Keyboard keyboard;
+
+    private void Start()
+    {
+        mouse = Mouse.current;
+        keyboard = Keyboard.current;
+    }
+
+    public void Switch()
+    {
+        player.SetActive(false);
+        prisonCamera.SetActive(true);
+        isPrisonCameraUsed = true;
+    }
+
+    private void Update()
+    {
+        if (isPrisonCameraUsed && (mouse.leftButton.wasPressedThisFrame || keyboard.anyKey.wasPressedThisFrame))
+        {
+            player.SetActive(true);
+            prisonCamera.SetActive(false);
+            isPrisonCameraUsed = false;
+        }
+    }
+}
